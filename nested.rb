fea1bd06354b4081978c13b1095bba56f0fd7dbb
Evan Jeffries <ejeffries14@gmail.com>
2019-07-22 08:52:21 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,7 +83,6 @@ programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby",:languages => ["LIS
 }
    #:known_for => "Ruby",
     #:languages => ["LISP", "C"]
-    
     programmer_hash
 end
 
@@ -106,7 +105,9 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-    programmer_hash[:alan_kay][:known_for] = alans_new_info 
+    programmer_hash[:alan_kay][:known_for] = "GUI" 
+    
+    programmer_hash
 end
 
 def adding_to_dennis
